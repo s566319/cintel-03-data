c75870bcae7e171b0091b8d8bea1a8260a280044
Inga Miller <s566319@nwmissouri.edu>
2023-07-14 13:57:53 -0500

    Iris Commit

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ from penguins_server import get_penguins_server_functions
 from penguins_ui_inputs import get_penguins_inputs
 from penguins_ui_outputs import get_penguins_outputs
 
+from iris_server import get_iris_server_functions
+from iris_ui_inputs import get_iris_inputs
+from iris_ui_outputs import get_iris_outputs
+
 from util_logger import setup_logger
 
 logger, logname = setup_logger(__name__)
@@ -49,9 +53,12 @@ app_ui = ui.page_navbar(
                     ),
                     ui.tags.li(
                         "To explore the Penguins Dataset, click the 'Penguins' tab."
+                    )
+                    ui.tags.li(
+                        "To explore the Iris Dataset, click the 'Iris' tab."
                     ),
-                ),
-                ui.tags.hr(),
+                ),   
+                ui.tags.hr(),         
                 ui.h2("Main Panel with Reactive Output"),
                 ui.tags.hr(),
                 ui.output_text_verbatim("welcome_output"),
@@ -73,6 +80,13 @@ app_ui = ui.page_navbar(
             get_penguins_inputs(),
             get_penguins_outputs(),
         ),
+     ),
+    ui.nav(
+        "Iris",
+        ui.layout_sidebar(
+            get_iris_inputs(),
+            get_iris_outputs(),
+        ),
     ),
     ui.nav(ui.a("About", href="https://github.com/s566319")),
     ui.nav(ui.a("GitHub", href="https://github.com/s566319/cintel-03-data")),
@@ -90,7 +104,7 @@ def server(input, output, session):
     @render.text
     def welcome_output():
         user = input.name_input()
-        welcome_string = f'Welcome to this page! {user}'
+        welcome_string = f"Welcome to this page, {user}!"
         return welcome_string
 
     @output
@@ -103,6 +117,7 @@ def server(input, output, session):
 
     get_mtcars_server_functions(input, output, session)
     get_penguins_server_functions(input, output, session)
-
+    get_iris_server_functions(input, output, session)
 
 app = App(app_ui, server)
+
